00327831896db5f2bbefbdc8527d48551505ae7b
Administrator <Administrator@20170207-150902>
2017-09-07 15:09:31 +0800

    角色简单管理和service注解管理

---
+++ src/main/java/com/jlqr/common/Log4jUtils.java
@@ -0,0 +1,8 @@
+package com.jlqr.common;
+
+import com.jfinal.log.Log;
+
+public class Log4jUtils {
+	public static final Log logger = Log.getLog(Log4jUtils.class);
+//	public static final Logger logger = Logger.getLogger("");
+}
