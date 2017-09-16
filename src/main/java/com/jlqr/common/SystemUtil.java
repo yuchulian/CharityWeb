ce4bfac72e2c0e324ffab8d853ca861b5ce604e8
Administrator <Administrator@20170207-150902>
2017-09-16 09:41:57 +0800

    添加公共方法

--- src/main/java/com/jlqr/common/SystemUtil.java
+++ src/main/java/com/jlqr/common/SystemUtil.java
@@ -1,5 +1,8 @@
 package com.jlqr.common;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import org.activiti.engine.ProcessEngine;
@@ -21,4 +24,27 @@ public class SystemUtil {
 		UUID uuid = UUID.randomUUID();
 		return uuid.toString().replace("-", "");
 	}
+
+	/**
+	 * 判断类似子集，如:<br>
+	 * A:",1,2,3,"<br>
+	 * B:",2,3,"<br>
+	 * spliRegex:","<br>
+	 * B是A的子集返回true
+	 * @param a
+	 * @param b
+	 * @param spliRegex
+	 * @return
+	 */
+	public static boolean stringAContainB(String a, String b, String spliRegex){
+		List<String> lista = Arrays.asList(a.split(spliRegex));
+		List<String> listA = new ArrayList<String>(lista);
+		listA.remove("");
+        List<String> listb = Arrays.asList(b.split(spliRegex));
+        List<String> listB = new ArrayList<String>(listb);
+        listB.remove("");
+        boolean containsAll = listA.containsAll(listB);
+		return containsAll;
+	}
+	
 }
