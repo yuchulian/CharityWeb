6cd485e8fa4cdc2573ac6bbf2715daa9d6dec12f
LiQiRan <1196273318@qq.com>
2017-09-26 14:44:59 +0800

    添加费用报销

--- src/main/java/com/jlqr/common/model/_MappingKit.java
+++ src/main/java/com/jlqr/common/model/_MappingKit.java
@@ -21,6 +21,7 @@ public class _MappingKit {
 		arp.addMapping("login_info", "id", LoginInfo.class);
 		arp.addMapping("power_info", "id", PowerInfo.class);
 		arp.addMapping("project_info", "id", ProjectInfo.class);
+		arp.addMapping("reimburse_info", "id", ReimburseInfo.class);
 		arp.addMapping("role_info", "id", RoleInfo.class);
 	}
 }
