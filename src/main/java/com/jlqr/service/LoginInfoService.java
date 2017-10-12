cb3fdee6b70698fc823f7ed6bca10ea6f197d285
yuchulian <1490798238@qq.com>
2017-10-12 19:52:08 +0800

    增加密码重置功能

--- src/main/java/com/jlqr/service/LoginInfoService.java
+++ src/main/java/com/jlqr/service/LoginInfoService.java
@@ -2,6 +2,8 @@ package com.jlqr.service;
 
 import java.util.Date;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import com.jlqr.common.ServiceUtil;
 import com.jlqr.common.model.LoginInfo;
 
@@ -19,6 +21,14 @@ public class LoginInfoService extends ServiceUtil{
 	
 	public LoginInfo findLoginInfoById(Integer id) throws Exception{
 		return LoginInfo.dao.findById(id);
+	}
+
+	public LoginInfo findLoginInfoByIdAndPassword(Integer id, String oldPassword) {		
+		return LoginInfo.dao.findFirst("select * from login_info where login_pwd='"+DigestUtils.md5Hex(oldPassword)+"' and id ="+id);
+	}
+
+	public void resetPwdUpdate(LoginInfo loginInfo) {
+		loginInfo.update();
 	}
 	
 	//保存登录信息
